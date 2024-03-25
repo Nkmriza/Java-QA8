86e9a5ad365616f910b45a9ae8642844ffdd37db
user <lizasamsung98@gmail.com>
2024-03-25 23:17:14 +0200

    HW2

--- src/hw1/MainTast4.java
+++ src/hw1/MainTast4.java
@@ -4,8 +4,8 @@ public class MainTast4 {
     public static void main(String[] args) {
         int a = 7;
         int b = 9;
-        a = b++ *2 - ++a;
-        b = (a/b);
+        a = b++ * 2 - ++a;
+        b = (a / b) * 4;
 
         System.out.println(a);
         System.out.println(b);
