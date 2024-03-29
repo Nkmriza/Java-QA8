ac39c10971ef76231d882d9e3584beeceb3ec48b
user <lizasamsung98@gmail.com>
2024-03-29 06:52:01 +0200

    HW3 t1 - t5

--- src/hw3/HW3t6.java
+++ src/hw3/HW3t6.java
@@ -1,5 +1,8 @@
 package hw3;
 
+import java.lang.classfile.instruction.SwitchCase;
+import java.util.Scanner;
+
 public class HW3t6 {
     /*Використовуючи оператор switch написати програму, яка виводить на консоль
      посилання для завантаження програми. З вибору програм взяти: IntelliJ IDEA,
@@ -10,6 +13,26 @@ public class HW3t6 {
       користувачем ОС немає, виводиться повідомлення в консоль, що такої ОС немає.
      */
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Яка програма вас цікавить?");
+        String prog = scanner.nextLine();
+        switch (prog.toLowerCase()){
+            case ("IntelliJ IDEA"):System.out.println("IntelliJ IDEA");
+            break;
+            case ("Git"):System.out.println("Git");
+            break;
+            case ("Java"):System.out.println("Java");
+            break;
+
+        default:
+            System.out.println("Такої програми не існує");
+        break;}
+
+
+
+      /*  System.out.println("Якою ОС ви користуєтесь?");
+         String oS = scanner.nextLine();*/
+
 
     }
 }
