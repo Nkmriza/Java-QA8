7617eae84025ec5c78c2e129268f76a7ed8ef260
user <lizasamsung98@gmail.com>
2024-04-02 13:50:04 +0300

    ch

--- src/hw4/HW4t4.java
+++ src/hw4/HW4t4.java
@@ -11,21 +11,29 @@ public class HW4t4 {
      3,2,1: На екрані має вивестися наступне повідомлення: “Федя буде йти до магазину о 14:00”
      Приклад2. після введення 1,2,3: На екрані має вивестися наступне повідомлення:
      "Маша йтиме в тренажерний зал о 14:00"*/
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String[] arrayName= {"Петя", "Маша", "Олена", "Федя","Саша","Антон","Гліб"};
-        int[] number = {10, 12, 14, 16, 18,20};
-        String [] arrayPlace = {"школу", "магазин", "церкву",
-     "тренажерний зал", "кіно", "поліклініку"};
+        String[] arrayName = {"Петя", "Маша", "Олена", "Федя", "Саша", "Антон", "Гліб"};
+        int[] number = {10, 12, 14, 16, 18, 20};
+        String[] arrayPlace = {"школу", "магазин", "церкву",
+                "тренажерний зал", "кіно", "поліклініку"};
         System.out.println("Введіть три цифри: ");
         String sentence = scanner.next();
+        /*String name1 = arrayName[1];
+        String name2 = arrayName[2];
+        String name3 = arrayName[3];
+        String name4 = arrayName[4];
+        String name5 = arrayName[5];
+        String name6 = arrayName[6];
+        String name7 = arrayName[7];
+        System.out.println(name1);*/
+
         int name = scanner.nextInt() - 1;
+        System.out.println(name);
        /* System.out.println(arrayName.length);
         System.out.println(number.length);
         System.out.println(arrayPlace.length);
         System.out.println(arrayName[0].length() + "буде йти в " + arrayPlace[0].length() + "о " + number[0] );*/
-
-
 
 
     }
