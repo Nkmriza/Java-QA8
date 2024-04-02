7661ff00d4404e5b63718ff1d32d932c7a701d9f
user <lizasamsung98@gmail.com>
2024-04-02 13:49:58 +0300

    ch

--- src/hw4/HW4t5.java
+++ src/hw4/HW4t5.java
@@ -1,11 +1,37 @@
 package hw4;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class HW4t5 {
     /*Є одновимірний масив із 10 елементів, заповнений випадковими числами.
      Користувач вводить із клавіатури число. Програма показує чи є таке число
      у створеному раніше масиві.*/
-    public static void main(String[] args){
-        int[] number = new int[10];
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random numbers = new Random();
+        int[] numbersInArray = new int[10];
+        for (int i = 0; i < numbersInArray.length; i++) {
+            numbersInArray[i] = numbers.nextInt(20);
+        }
+        for (int randomNumbers : numbersInArray) {
+            System.out.print(randomNumbers + " ");
+        }
+        System.out.println(" ");
+        System.out.println("Введіть число:");
+        int certainNumber = scanner.nextInt();
+        boolean answer = false;
+        for (int i = 0; i < numbersInArray.length; i++) {
+            if (numbersInArray[i] == certainNumber){
+                answer = true;
+                System.out.println("Число " + certainNumber + " " + "є у даному масиві");
 
+            } else {
+                System.out.println("Таке число відсутнє у даному масиві");
+            }
+            break;
+            /*я просто не понимаю, как это сделать*/
+        }
     }
+
 }
--- src/hw4/HW4t6.java
+++ src/hw4/HW4t6.java
@@ -1,12 +1,47 @@
 package hw4;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class HW4t6 {
     /*Заповнити масив на 45 елементів довільними числами від -50 до +50.
     Знайти мінімальний елемент та вивести його на консоль. Знайти максимальний
     елемент та вивести його на консоль.*/
     public static void main(String[] args) {
-        int[] number = new int[45];
+        /* Random randomObject = new Random();*/
+        int[] numbersAmount = new int[45];
+        for (int i = 0; i < numbersAmount.length; i++) {
+            numbersAmount[i] = new Random().nextInt(-50, 50);
+        }
+        int minNumber = numbersAmount[0];
+        int maxNumber = numbersAmount[0];
 
+        for (int i = 0; i < numbersAmount.length; i++) {
+            if (numbersAmount[i] < minNumber) {
+                minNumber = numbersAmount[i];
+            }
+        }
+
+        for (int i = 0; i < numbersAmount.length; i++) {
+            if (numbersAmount[i] > maxNumber) {
+                maxNumber = numbersAmount[i];
+            }
+        }
+
+        for (int number : numbersAmount) {
+            System.out.print(number + " ");
+        }
+        System.out.println(" ");
+        System.out.println(minNumber);
+        System.out.println(maxNumber);
+
+        System.out.println("============");
+        System.out.println(" ");
+        int minNumber2 = Arrays.stream(numbersAmount).min().getAsInt();
+        int maxNumber2 = Arrays.stream(numbersAmount).max().getAsInt();
+        System.out.println(minNumber2);
+        System.out.println(maxNumber2);
+        /* в гугле пишут, что можно еще таким способом*/
 
     }
 
