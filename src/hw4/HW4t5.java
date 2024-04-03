592edaedaf29a892017e2a625139703813676dd6
user <lizasamsung98@gmail.com>
2024-04-03 19:06:39 +0300

    hw4

--- src/hw4/HW4t5.java
+++ src/hw4/HW4t5.java
@@ -17,21 +17,24 @@ public class HW4t5 {
         for (int randomNumbers : numbersInArray) {
             System.out.print(randomNumbers + " ");
         }
+
         System.out.println(" ");
         System.out.println("Введіть число:");
         int certainNumber = scanner.nextInt();
         boolean answer = false;
-        for (int i = 0; i < numbersInArray.length; i++) {
-            if (numbersInArray[i] == certainNumber){
-                answer = true;
-                System.out.println("Число " + certainNumber + " " + "є у даному масиві");
 
-            } else {
-                System.out.println("Таке число відсутнє у даному масиві");
+        for (int i = 0; i < numbersInArray.length; i++) {
+            if (numbersInArray[i] == certainNumber) {
+                answer = true;
             }
-            break;
-            /*я просто не понимаю, как это сделать*/
         }
+
+        if (answer) {
+            System.out.println("Число " + certainNumber + " " + "є у даному масиві");
+        } else {
+            System.out.println("Таке число відсутнє у даному масиві");
+        }
+
     }
 
 }
