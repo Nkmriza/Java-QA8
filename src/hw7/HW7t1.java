a7cb1a3cfa2f9b1d95963291909a3c774234e213
user <lizasamsung98@gmail.com>
2024-04-13 21:32:17 +0300

    hw 7

--- src/hw7/HW7t1.java
+++ src/hw7/HW7t1.java
@@ -8,17 +8,21 @@ public class HW7t1 {
     І виводить на екран усі парні числа списком, а також непарні числа списком.
      */
     public static void arrays(int[] arr) {
+        System.out.print("Парні числа: ");
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] % 2 == 0) {
-                System.out.println("Парні числа:" + arr[i]);
+                System.out.print(arr[i] + " ");
             }
+        }
+        System.out.println();
+        System.out.print("Непарні числа: ");
+            for (int i = 0; i < arr.length; i++){
             if (arr[i] % 2 != 0) {
-                System.out.println("Непарні числа:" + arr[i]);
+                System.out.print(arr[i] + " " );
             }
         }
     }
-
-    public static void main(String [] args) {
+    public void main(String [] args) {
         HW7t1 variable = new HW7t1();
         int[] numbersInArray = {1,2,3,4,5,6,7,8,9,10};
         /*arrays(numbersInArray);*/
