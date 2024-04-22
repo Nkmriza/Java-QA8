2edf408038dac43a429d1dd91d31793e66adc164
user <lizasamsung98@gmail.com>
2024-04-22 21:39:44 +0300

    t2

--- src/hw10/arrayTask2/Array.java
+++ src/hw10/arrayTask2/Array.java
@@ -1,5 +1,8 @@
 package hw10.arrayTask2;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Array {
     /*) Створити метод, у якому створюється одновимірний масив типу int довільного
     розміру від 1 до 30, в якому кожному елементу масиву надається довільне значення
@@ -10,5 +13,26 @@ public class Array {
 виняткової ситуації створити власний клас checked винятки. Прописати у методі умови генерації
 даних винятків, і навіть вказати в сигнатурі даного методу.
 Обробку виключення здійснити в сторонньому класі Main в якому потрібно викликати даний метод.*/
-    public int [] array;
+    private int[] array;
+
+    public int[] getArray() {
+        return array;
+    }
+
+    public Array(int[] array) {
+        this.array = array;
+    }
+
+    public void randomIndex() {
+        Random random = new Random();
+        int sizeOfArray = random.nextInt(30) + 1;
+        int[] array = new int[sizeOfArray];
+
+        for (int i = 0; i < sizeOfArray; i++) {
+            array[i] = random.nextInt(31);
+        }
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введіть індекс від 0 до 30");
+        int index = scanner.nextInt();
+    }
 }
