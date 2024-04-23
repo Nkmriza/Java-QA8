740647446b6ef7c4e6a151b9ee0e84ac98803f78
user <lizasamsung98@gmail.com>
2024-04-23 23:28:57 +0300

    t1... Т_Т

---
+++ src/hw10/arrayTask2/ArrayMain.java
@@ -0,0 +1,8 @@
+package hw10.arrayTask2;
+
+public class ArrayMain {
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ src/hw10/shopTask1/BuyingProducts.java
@@ -0,0 +1,4 @@
+package hw10.shopTask1;
+
+public interface BuyingProducts{
+   void returnCost();}
