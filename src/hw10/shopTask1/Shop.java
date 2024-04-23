dfb1c3f81190ba597355d86c390067e75f3e4e4c
user <lizasamsung98@gmail.com>
2024-04-23 21:59:57 +0300

    t2

--- src/hw10/shopTask1/Shop.java
+++ src/hw10/shopTask1/Shop.java
@@ -18,17 +18,93 @@ public class Shop {
 
     }
 
-
-
-    @Override
-    public String toString() {
-        return "Shop{" +
-                "shopName='" + shopName + '\'' +
-                ", priceOfPurchase=" + priceOfPurchase +
-                ", customerName='" + customerName + '\'' +
-                ", customerAge=" + customerAge +
-                ", sellerName='" + sellerName + '\'' +
-                ", productName='" + productName + '\'' +
-                '}';
+    public String getShopName() {
+        return shopName;
     }
+
+    public void setShopName(String shopName) {
+        this.shopName = shopName;
+    }
+
+    public int getPriceOfPurchase() {
+        return priceOfPurchase;
+    }
+
+    public void setPriceOfPurchase(int priceOfPurchase) {
+        this.priceOfPurchase = priceOfPurchase;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public int getCustomerAge() {
+        return customerAge;
+    }
+
+    public void setCustomerAge(int customerAge) {
+        this.customerAge = customerAge;
+    }
+
+    public String getSellerName() {
+        return sellerName;
+    }
+
+    public void setSellerName(String sellerName) {
+        this.sellerName = sellerName;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public void returnCost(String shopName, int priceOfPurchase,
+                                        String customerName, int customerAge,
+                                        String sellerName, String productName) {
+        /*String alcohol = "алкогольний напій";
+        String nonAlcohol = "безалкогольний напій";
+        if (productName.equalsIgnoreCase(alcohol)) {
+            System.out.println("Товар алкогольний");
+        }
+        if (productName.equalsIgnoreCase(nonAlcohol)) {
+            System.out.println("Товар безалкогольний");
+        }*/
+
+      boolean consistsAlcohol = false;
+      boolean honestSeller = false;
+        if (consistsAlcohol) {
+            if (sellerName.equals("чесний")) {
+                System.out.println( "Продавець не продасть покупцю" + customerName + " алкогольний товар." );
+            } else if (customerAge < 18 && consistsAlcohol == true){
+                System.out.println("Продавець порушує правила продажу.");
+            }
+            else {
+                System.out.println("Товар не містить алкоголь. Вдала покупка!");
+            }
+        }
+
+       /* if (sellerName.equalsIgnoreCase("чесний")) {
+            if (productName.equalsIgnoreCase(alcohol)) {
+                System.out.println("Продавець чесний і відмовився продавати алкоголь.");
+            } else if (productName == nonAlcohol) {
+                System.out.println(nonAlcohol);
+            }
+        } else {
+            if (productName.equalsIgnoreCase(alcohol)) {
+                System.out.println("Продавець нечесний і продасть вам алкогольний товар.");
+            } else if (productName.equalsIgnoreCase(nonAlcohol)) {
+                System.out.println("Товар безалкогольний.");
+            }
+        }*/
+        double discount = 0.1;
+    }
+
 }
